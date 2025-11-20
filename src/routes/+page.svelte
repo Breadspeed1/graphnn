459cb3db90649fc596041d7120b926ecdcbe116f
Aiden Voth <aiden@voth.name>
2025-11-19 21:59:55 -0500

    all the things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,32 @@
-<main class="container">
-  <h1>Yo yo yo</h1>
-</main>
+<script>
+  import { SvelteFlow, Background } from "@xyflow/svelte";
+
+  import "@xyflow/svelte/dist/style.css";
+
+  let nodes = $state.raw([
+    {
+      id: "1",
+      position: { x: 0, y: 0 },
+      data: { label: "Hello" },
+    },
+    {
+      id: "2",
+      position: { x: 100, y: 100 },
+      data: { label: "World" },
+    },
+  ]);
+</script>
+
+<div class="container">
+  <SvelteFlow bind:nodes fitView>
+    <Background />
+  </SvelteFlow>
+</div>
+
+<style>
+  .container {
+    margin: 0;
+    padding: 0;
+    height: 100dvh;
+  }
+</style>
